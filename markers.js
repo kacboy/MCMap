9b1810dd5c2e0b5bd99ebd840de01d83ecb7e6e6
Marcus Blackstock <marcusblackstock@gmail.com>
2020-06-11 14:31:25 -0700

    Added Markers

---
+++ markers.js
@@ -0,0 +1,25 @@
+var markers={
+  "normalrender": [
+    {
+      "checked": false,
+      "createInfoWindow": true,
+      "displayName": "Bases",
+      "groupName": "Bases1a73_9b5b",
+      "icon": "icons/marker_tower_red.png"
+    },
+    {
+      "checked": false,
+      "createInfoWindow": true,
+      "displayName": "Towns",
+      "groupName": "Townse585_9b5b",
+      "icon": "icons/marker_town_red.png"
+    },
+    {
+      "checked": false,
+      "createInfoWindow": true,
+      "displayName": "Builds",
+      "groupName": "Buildse596_9b5b",
+      "icon": "icons/marker_factory.png"
+    }
+  ]
+};
---
+++ markersDB.js
@@ -0,0 +1,97 @@
+var markersDB={
+  "Bases1a73_9b5b": {
+    "created": false,
+    "name": "Bases",
+    "raw": [
+      {
+        "hovertext": "The Ranch\n",
+        "text": "The Ranch\n",
+        "x": -188,
+        "y": 74,
+        "z": -91
+      },
+      {
+        "hovertext": "BBC\n",
+        "text": "BBC\n",
+        "x": -203,
+        "y": 100,
+        "z": -423
+      },
+      {
+        "hovertext": "Mine Pyramid\n",
+        "text": "Mine Pyramid\n",
+        "x": -212,
+        "y": 88,
+        "z": 13
+      }
+    ]
+  },
+  "Buildse596_9b5b": {
+    "created": false,
+    "name": "Builds",
+    "raw": [
+      {
+        "hovertext": "Spawn Castle\n",
+        "text": "Spawn Castle\n",
+        "x": -203,
+        "y": 107,
+        "z": -183
+      },
+      {
+        "hovertext": "BBrC\n",
+        "text": "BBrC\n",
+        "x": -73,
+        "y": 100,
+        "z": -245
+      },
+      {
+        "hovertext": "Floating Island\n",
+        "text": "Floating Island\n",
+        "x": 35,
+        "y": 225,
+        "z": -189
+      },
+      {
+        "hovertext": "Apartment\n",
+        "text": "Apartment\n",
+        "x": -100,
+        "y": 165,
+        "z": -108
+      },
+      {
+        "hovertext": "Modern Home II\n",
+        "text": "Modern Home II\n",
+        "x": -273,
+        "y": 86,
+        "z": -73
+      },
+      {
+        "hovertext": "Giant Pyramid\n",
+        "text": "Giant Pyramid\n",
+        "x": -443,
+        "y": 146,
+        "z": -59
+      }
+    ]
+  },
+  "Townse585_9b5b": {
+    "created": false,
+    "name": "Towns",
+    "raw": [
+      {
+        "hovertext": "Sil's Town\n",
+        "text": "Sil's Town\n",
+        "x": -1516,
+        "y": 74,
+        "z": -261
+      },
+      {
+        "hovertext": "Waterfront Village\n",
+        "text": "Waterfront Village\n",
+        "x": 1309,
+        "y": 64,
+        "z": -1870
+      }
+    ]
+  }
+};
